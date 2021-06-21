c30d41873fd8753604bfccaf5edef390a4f20240
balker0322 <email@email>
2021-06-21 18:37:57 +0800

    update

--- main.py
+++ main.py
@@ -7,37 +7,48 @@ from tradecalc import *
 def update_user_params(**kwargs):
     if kwargs['pr']:
         set_pair(kwargs['pr'])
+
+    pair = get_pair()    
+
     if kwargs['rp']:
         set_risk_as_percent(kwargs['rp'])
     if kwargs['sl']:
-        set_sl(kwargs['sl'])
+        set_target_stop_loss(pair, kwargs['sl'])
     if kwargs['e']:
-        set_target_entry(kwargs['e'])
-    if (kwargs['rp']):
-        set_risk_as_percent(kwargs['rp'])
-    if not (kwargs['rv'] == 1.0):
-        pass
-
-    # min_lot_size = get_min_lot_size(pair)
-    # max_position_size = calc_position_size(
-    #     entry_price=get_target_entry(pair),
-    #     stop_loss_price=get_target_stop_loss(pair),
-    #     min_lot_size=min_lot_size,
-    #     risk=get_risk()
-    # )
-
-
+        set_target_entry(pair, kwargs['e'])
+    if kwargs['sl'] or  kwargs['e'] or  kwargs['rp']:
+        min_lot_size = get_min_lot_size(pair)
+        max_position_size = calc_position_size(
+            entry_price=get_target_entry(pair),
+            stop_loss_price=get_target_stop_loss(pair),
+            min_lot_size=min_lot_size,
+            risk=get_risk()
+        )
+        set_max_position_size(pair, max_position_size)
+        set_position_size(pair, max_position_size)
+    if kwargs['rv']:
+        set_position_size(
+            pair,
+            round_param(
+                get_max_position_size(pair)* kwargs['rv'],
+                get_min_lot_size(pair)
+            )
+        )
 
 @click.group()
-def main():
-    pass
+@click.option('-pr',default='')
+@click.option('-e',default=0.0)
+@click.option('-sl',default=0.0)
+@click.option('-rp',default=0.0)
+@click.option('-rv',default=0.0)
+def main(**kwargs):
+    update_user_params(**kwargs)
 
 # experiment
 @main.command()
 @click.option('-q', multiple=True)
 def exp(q):
     print(q)
-
 
 # Set Capital
 @main.command()
@@ -51,96 +62,8 @@ def c(capital):
         total += float(cap)
     set_capital(total)
 
-# set pair
-@main.command()
-@click.argument('pr')
-def pr(pr):
-    '''
-    Set Pair
-    '''
-    set_pair(pr.upper())
-
-# Set Risk Percentage
-@main.command()
-@click.argument('risk_percentage')
-def rp(risk_percentage):
-    '''
-    Set risk in terms of percent of capital
-    '''
-    set_risk_as_percent(risk_percentage)
-
-# Set Target Entry
-@main.command()
-@click.argument('pair')
-@click.argument('target_entry')
-def e(pair, target_entry):
-    '''
-    Set Target Entry
-    '''
-    set_target_entry(pair, round_param(target_entry,get_min_price_step(pair)))
-
-# Set Target Stop Loss
-@main.command()
-@click.argument('pair')
-@click.argument('target_stop_loss')
-def sl(pair, target_stop_loss):
-    '''
-    Set Target Stop Loss
-    '''
-    set_target_stop_loss(pair, round_param(target_stop_loss,get_min_price_step(pair)))
-
-# Calculate Max Position
-@main.command()
-@click.argument('pair')
-@click.option('-p',default=1.0)
-def ps(pair, p):
-    '''
-    Set Position Size
-    '''
-    min_lot_size = get_min_lot_size(pair)
-    max_position_size = calc_position_size(
-        entry_price=get_target_entry(pair),
-        stop_loss_price=get_target_stop_loss(pair),
-        min_lot_size=min_lot_size,
-        risk=get_risk()
-    )
-    print('{} max_position_size is {}'.format(pair, max_position_size))
-    set_position_size(pair, round_param(max_position_size*p,min_lot_size))
-
-# open position
-@main.command()
-@click.argument('et')
-@click.argument('s')
-@click.option('-pr',default='')
-@click.option('-e',default=0.0)
-@click.option('-sl',default=0.0)
-@click.option('-rp',default=0.0)
-@click.option('-rv',default=1.0)
-@click.option('-tp', multiple=True)
-@click.option('-tprr', multiple=True)
-def open(et, s, **kwargs):
-    entry_type = et.lower()
-    side = s.lower()
-
-    update_user_params(**kwargs)
-
-    if entry_type == 'm' and side == 'long':
-        mlong()
-    elif entry_type == 'l' and side == 'long':
-        llong()
-    elif entry_type == 'sl' and side == 'long':
-        sllong()
-    elif entry_type == 'm' and side == 'short':
-        mshort()
-    elif entry_type == 'l' and side == 'short':
-        lshort()
-    elif entry_type == 'sl' and side == 'short':
-        slshort()
-    else:
-        error_print('Invalid arguments')
-        return
-
 # Market Long Entry
+@main.command()
 def mlong():
     '''
     Market Long Entry
@@ -150,6 +73,7 @@ def mlong():
     market_long_entry(pair, position_size)
 
 # Market Short Entry
+@main.command()
 def mshort():
     '''
     Market Short Entry
@@ -160,6 +84,7 @@ def mshort():
     market_short_entry(pair, position_size)
 
 # Limit Long Entry
+@main.command()
 def llong():
     '''
     Limit Long Entry
@@ -169,6 +94,7 @@ def llong():
     limit_long_entry(pair, position_size, entry_price)
 
 # Limit Short Entry
+@main.command()
 def lshort():
     '''
     Limit Short Entry
@@ -179,6 +105,7 @@ def lshort():
     limit_short_entry(pair, position_size, entry_price)
 
 # Stop Limit Long Entry
+@main.command()
 def sllong():
     '''
     Stop Limit Long Entry
@@ -189,6 +116,7 @@ def sllong():
     stop_limit_long_entry(pair, position_size, entry_price)
 
 # Stop Limit Short Entry
+@main.command()
 def slshort():
     '''
     Stop Limit Short Entry
@@ -201,19 +129,15 @@ def slshort():
 
 # Set take profit
 @main.command()
-@click.argument('pair')
-@click.argument('tp1', default=0.0)
-@click.argument('tp2', default=0.0)
-@click.argument('tp3', default=0.0)
-@click.argument('tp4', default=0.0)
-@click.argument('tp5', default=0.0)
-def tp(pair, **kwargs):
+@click.argument('tp_list', nargs=-1)
+def tp(tp_list):
     '''
     Set take profit
     '''
+    pair = get_pair()
     min_price_step = get_min_price_step(pair)
     take_profit_list = []
-    for tp in kwargs:
+    for tp in tp_list:
         if float(tp) == 0.0:
             continue
         take_profit_list.append(round_param(tp, min_price_step))
@@ -222,51 +146,47 @@ def tp(pair, **kwargs):
 
 # Set take profit as rr ratio
 @main.command()
-@click.argument('pair')
-@click.argument('rr1', default=0.0)
-@click.argument('rr2', default=0.0)
-@click.argument('rr3', default=0.0)
-@click.argument('rr4', default=0.0)
-@click.argument('rr5', default=0.0)
-def tprr(pair, **kwargs):
+@click.argument('rr_list', nargs=-1)
+def tprr(rr_list):
     '''
     Set take profit as rr ratio
     '''
+    pair = get_pair()
     min_price_step = get_min_price_step(pair)
     entry_price = get_target_entry(pair)
     stop_loss_price = get_target_stop_loss(pair)
     take_profit_list = []
-    for rr in kwargs:
-        if float(kwargs[rr]) == 0.0:
+    for rr in rr_list:
+        if float(rr) == 0.0:
             continue
-        take_profit_list.append(calc_exit_price(entry_price, stop_loss_price, min_price_step, kwargs[rr]))
+        take_profit_list.append(calc_exit_price(entry_price, stop_loss_price, min_price_step, rr))
     set_take_profit(pair, take_profit_list)
 
 # Close Open Position
 @main.command()
-@click.argument('pair')
-def cp(pair):
+def cp():
     '''
     Close Open Position
     '''
+    pair = get_pair()
     close_open_position(pair)
 
 # Cancel Open Orders
 @main.command()
-@click.argument('pair')
-def co(pair):
+def co():
     '''
     Cancel Open Orders
     '''
+    pair = get_pair()
     cancel_all_order(pair)
 
 # Submit SL and TP
 @main.command()
-@click.argument('pair')
-def sltp(pair):
+def sltp():
     '''
     Submit SL and TP
     '''
+    pair = get_pair()
     position = float(get_open_position(pair)['positionAmt'])
     if position == 0.0:
         print('No open position for {} pair'.format(pair))
@@ -295,11 +215,11 @@ def sltp(pair):
 
 # Calculate exit prices
 @main.command()
-@click.argument('pair')
-def calcrr(pair):
+def calcrr():
     '''
     Calculate exit prices
     '''
+    pair = get_pair()
     target_entry = get_target_entry(pair)
     target_exit = get_target_stop_loss(pair)
     min_price_step = get_min_price_step(pair)
@@ -307,11 +227,11 @@ def calcrr(pair):
     side = 'LONG' if target_exit < target_entry else 'SHORT'
     print('========================================')
     print(pair)
-    print('{} ENTRY: {} USDT'.format(pair,side,target_entry))
+    print('{} ENTRY: {} USDT'.format(side,target_entry))
     for rr in rr_ratio:
         price = calc_exit_price(entry_price=target_entry, stop_loss_price=target_exit, min_price_step=min_price_step, rr_ratio=rr)
         s = 'RR_RATIO: {:.1f}'.format(float(rr)) if float(rr) > 0.0 else ('STOP LOSS' if float(rr) < 0.0 else 'BREAKEVEN')
-        print('{}\t{}'.format(
+        print('{}\t{} USDT'.format(
             s,
             price
         ))
@@ -320,17 +240,27 @@ def calcrr(pair):
 
 # Show current status
 @main.command()
-@click.argument('pair')
-def stat(pair):
+def stat():
     '''
     Show current status
     '''
-    print(pair)
+    pair = get_pair()
+    print('========================================')
+    print('Capital: {} USDT'.format(get_capital()))
+    print('Risk: {0:.2f}%'.format(get_risk_as_percent()*100.0))
+    print()
+    print('Pair: {}'.format(pair))
+    print('Target Entry:        {} USDT'.format(get_target_entry(pair)))
+    print('Target Stop Loss:    {} USDT'.format(get_target_stop_loss(pair)))
+    print('Entry Position Size: {} {}'.format(get_position_size(pair),pair[:-4]))
+    print('Max Position Size:   {} {}'.format(get_max_position_size(pair),pair[:-4]))
+    print()
     print('Market Price: {} USDT'.format(get_market_price(pair)))
 
     position = get_open_position(pair)
     if float(position['positionAmt']) == 0.0:
         print('Position: None')
+        print('========================================')
         return
     
     entryPrice = position['entryPrice']
@@ -342,11 +272,11 @@ def stat(pair):
     sl_list = get_sl_order(pair)
     tp_list = get_tp_order(pair)
 
-    capital = get_capital(pair)
+    capital = get_capital()
 
     if sl_list:
         print('SL:')
-        print('{}\t{}\t{}'.fomat('price','qty','pnl'))
+        print('{}\t{}\t{}'.format('price','qty','pnl'))
         for sl in sl_list:
             price = sl['stopPrice']
             qty = sl['origQty']
@@ -358,7 +288,7 @@ def stat(pair):
 
     if tp_list:
         print('TP:')
-        print('{}\t{}\t{}'.fomat('price','qty','pnl'))
+        print('{}\t{}\t{}'.format('price','qty','pnl'))
         for sl in sl_list:
             price = sl['price']
             qty = sl['origQty']
@@ -367,6 +297,8 @@ def stat(pair):
             print('{} USDT\t{} {}\t{}'.format(price,qty,pair[-4:],pnl))
     else:
         print('TP: No set Take Profit')
+
+    print('========================================')
 
 
 if __name__ == '__main__':
